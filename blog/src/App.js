e0dadb0853a61243750753e473ac455f2f0f3512
Seoyun <sylph0105@naver.com>
2022-03-18 15:39:52 +0900

    [Update] blog. React 에서 state 사용해보기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStarAndCrescent } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
+  // * 구조 분해 문법(Destructuring Assignment)
+  // let [a, b] = useState('c') 형태로 사용하면
+  // useState('c') = [state 데이터, state 데이터 변경 함수] 와 동일
+
+  // * state 는 자주 바뀌는 중요한 데이터들을 저장하기에 용이
+  // 1. 변수 대신 사용하는 데이터 저장공간
+  // 2. useState() 를 이용해 만들어야 함
+  // 3. 문자, 숫자, array, object 다 저장 가능
+
+  let [title, setTitle] = useState(['[React] React 설치와 개발환경 셋팅하기', '[React]  JSX 를 이용해 HTML 페이지 제작해보기'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="veryperi-nav">
+        <div>
+          <FontAwesomeIcon icon={faStarAndCrescent} /> Seoyun's Blog
+          </div>
+      </div>
+        <h2 className='intro'>배움에서 가치를 찾는 예비 개발자입니다</h2>
+        <div className='list'>
+          <h3>{title[0]}</h3>
+          <p>2022-03-18</p>
+          <hr/>
+        </div>
+        <div className='list'>
+          <h3>{title[1]}</h3>
+          <p>2022-03-18</p>
+          <hr/>
+        </div>
     </div>
   );
 }
