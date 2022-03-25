5d59a2100889405b950d66891652864d4589c72a
Seoyun <sylph0105@naver.com>
2022-03-26 02:14:43 +0900

    [Update] shop. React 에서 Ajax 요청하기_2

--- shop/src/main.js
+++ shop/src/main.js
@@ -1,7 +1,25 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import { version } from 'styled-components';
+
+
+let Loading = styled.div`
+  font-size : 20px;
+  font-weight : bold;
+  color: #484848;
+  padding: 15px 20px;
+  background : #eee;
+  border-radius : 5px;
+  margin : 10px 0;
+`
 
 function Main(props) {
+
+  let [count, setCount] = useState(2);
+  let [load, setLoad] = useState(false);
+  let [loadFail, setLoadFail] = useState(false);
+  let [view, setView] = useState(true);
 
     return (
         <>
@@ -15,6 +33,7 @@ function Main(props) {
 
         {/* Grid */}
         <div className="container">
+
           <div className="row">
 
             {
@@ -26,18 +45,42 @@ function Main(props) {
             }
 
           </div>
-          <button className='btn btn-primary' onClick={()=>{ 
-            axios.get('https://codingapple1.github.io/shop/data2.json')
-            .then((result)=>{
-              console.log(result.data);
-              let newData = result.data;
-              props.setShoes([...props.shoes, ...newData]);
-              console.log('상품 데이터를 불러오는 데에 성공했습니다.');
-            })
-            .catch(()=>{
-              console.log('상품 데이터를 불러오는 데에 실패했습니다.')
-            });
-           }}>상품 더보기</button>
+          
+          {
+            load == true ? (<Loading>로딩중입니다...</Loading>) : null
+          }
+
+          {
+            loadFail == true ? (<Loading>로딩에 실패했습니다.</Loading>) : null
+          }
+          
+          {
+            view == true 
+            ?
+            (
+              <button className='btn btn-primary' onClick={()=>{ 
+                setLoad(true);
+                axios.get(`https://codingapple1.github.io/shop/data${count}.json`)
+                .then((result)=>{
+                  setLoad(false);
+                  setCount(count+1);
+                  // console.log(count);
+                  // console.log(result.data);
+                  let newData = result.data;
+                  props.setShoes([...props.shoes, ...newData]);
+                  console.log('상품 데이터를 불러오는 데에 성공했습니다.');
+                })
+                .catch(()=>{
+                  setLoad(false);
+                  setView(false);
+                  setLoadFail(true);
+                  console.log('상품 데이터를 불러오는 데에 실패했습니다.')
+                });
+               }}>상품 더보기</button>
+            )
+            : null
+          }
+
         </div>
         </>
     )
